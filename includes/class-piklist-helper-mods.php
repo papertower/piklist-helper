b54a4d6e52ab0637d0aac2010861de3c243852f9
Jason Adams <jason.the.adams@gmail.com>
2016-06-17 11:59:50 -0700

    updates

--- includes/class-piklist-helper-mods.php
+++ includes/class-piklist-helper-mods.php
@@ -5,7 +5,7 @@
 class PiklistHelperMods {
   public static function _construct() {
     // Add constant and variable type support
-    add_filter( 'piklist_part_add', array(__CLASS__, 'add_variable_support'), 1, 2 );
+    add_filter( 'piklist_part_process', array(__CLASS__, 'add_variable_support'), 1, 2 );
   }
 
   /**
@@ -13,8 +13,8 @@ class PiklistHelperMods {
    * Example: {my_var}    <-- Returns value of $my_var in scope or global
    * @since 0.1.0
    */
-  public static function add_variable_support($data) {
-    if ( !apply_filters('use_piklist_helper_mods', true) ) return $data;
+  public static function add_variable_support($part, $folder) {
+    if ( !apply_filters('use_piklist_helper_mods', true) ) return $part;
 
     $apply_constant = function($matches) {
       return constant($matches[1]);
@@ -25,14 +25,14 @@ class PiklistHelperMods {
       if ( isset($GLOBALS[$matches[1]]) ) return $GLOBALS[$matches[1]];
     };
 
-    $keys = array('type', 'template', 'taxonomy', 'capability', 'role');
+    $keys = array('post_type', 'template', 'taxonomy', 'capability', 'role');
     foreach($keys as $index => $key) {
-      if ( empty($data[$key]) ) continue;
-      $data[$key] = preg_replace_callback('/\[(\w+)\]/', $apply_constant, $data[$key]);
-      $data[$key] = preg_replace_callback('/{(\w+)}/',   $apply_variable, $data[$key]);
+      if ( empty($part['data'][$key]) ) continue;
+      $part['data'][$key] = preg_replace_callback('/\[(\w+)\]/', $apply_constant, $part['data'][$key]);
+      $part['data'][$key] = preg_replace_callback('/{(\w+)}/',   $apply_variable, $part['data'][$key]);
     }
 
-    return $data;
+    return $part;
   }
 }
 ?>
